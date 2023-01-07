76bdc78f8d8ec9e007e258a6bd0853e592198ddd
gabriel <gabriel.r.lacerda@hotmail.com>
2023-01-07 13:18:38 -0300

    cadastrando user

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,5 +1,6 @@
+const knex = require('../database/knex')
 const AppError = require('../utils/AppError')
-
+const { hash } = require('bcryptjs')
 class UsersController {
   async create(req, res) {
     const { name, email, password } = req.body
@@ -7,8 +8,24 @@ class UsersController {
     if (!name || !email || !password) {
       throw new AppError('Missing required fields', 400)
     }
+    // tirando espaços do email
+    const Email = email.trim()
 
-    return res.json({ name, email, password })
+    const checkUsersExists = await knex('users').where({ email: Email }).first()
+
+    if (checkUsersExists) {
+      throw new AppError('Usuário ja cadastrado', 400)
+    }
+
+    const hashedPassword = await hash(password, 8)
+
+    await knex('users').insert({
+      name,
+      email: Email,
+      password: hashedPassword
+    })
+
+    return res.status(201).json()
   }
 }
 
