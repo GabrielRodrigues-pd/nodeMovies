97dd1970f71255b05f894ec9773042bdb69b1b0f
gabriel <gabriel.r.lacerda@hotmail.com>
2023-01-07 16:10:37 -0300

    usando auth update user

--- src/controllers/MoviesController.js
+++ src/controllers/MoviesController.js
@@ -5,24 +5,26 @@ class MoviesController {
     const { title, description, tags, rating } = req.body
     const { id } = req.params
 
+    const user = await knex('users').where({ id }).first()
+
     const movie_id = await knex('movies').insert({
       title,
       description,
       rating,
-      user_id: id
+      user_id: user.id
     })
 
     const tagsInsert = tags.map(name => {
       return {
         movie_id,
-        user_id,
+        id,
         name
       }
     })
 
     await knex('tags').insert(tagsInsert)
 
-    res.res.status(201).json()
+    return res.status(201).json()
   }
 }
 
